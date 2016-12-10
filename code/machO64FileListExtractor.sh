99d57744670b45a77c8d16ab8961930921512d7f
Razvan Deaconescu <razvan.deaconescu@cs.pub.ro>
2016-12-10 11:40:05 +0200

    Add script to extract list of MachO executable files

---
+++ code/machO64FileListExtractor.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+IFS=$'\n'
+
+count=0
+echoerr()
+{
+    echo "$@" 1>&2
+}
+echoerr $count
+
+for file in $(find /usr/libexec -type f); do
+    count=$((count + 1))
+    if ! (("$count" % 1000)); then
+        echoerr "$count"
+    fi
+    /usr/bin/otool -h "$file" 2> /dev/null | grep '0xfeedfacf' > /dev/null 2>&1
+    if test $? -ne 0; then
+        continue
+    fi
+    echo "$file"
+done
