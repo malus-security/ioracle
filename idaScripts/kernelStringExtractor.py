e16e71ff07f77912fee3a5770671383ebc87bc6c
alecdeshotels <ladeshot@ncsu.edu>
2017-01-06 12:51:10 -0500

    adding some ida scripts for extracting strings from dyld_cache and kernel. This script also specifies the segment the string was found in. This is useful for determining which library or kernel extension the string appears in.

---
+++ idaScripts/kernelStringExtractor.py
@@ -0,0 +1,23 @@
+import idc
+import idautils
+from idaapi import *
+
+f = open('kernelStrings.pl','w')
+
+#why does this say False?
+s = idautils.Strings(False)
+s.setup(strtypes=Strings.STR_C)
+for i, v in enumerate(s):
+    if v is None:
+        f.write("Failed to retrieve string index %d" % i)
+    else:
+        #f.write("%x: len=%d type=%d index=%d-> '%s'\n" % (v.ea, v.length, v.type, i, str(v)))
+	seg=idc.SegName(v.ea)
+	#I'm probably replacing more things than I should, but prolog is getting confused by random backslashes.
+	currentString=str(v).replace("\n","").replace('"',"'").replace("\\","")
+	#f.write("%s\n" % str(seg))
+	f.write("kernelString(segment(\"%s\"),stringFromProgram(\"%s\")).\n" % (str(seg), currentString))
+
+f.close()
+
+idc.Exit(0)
