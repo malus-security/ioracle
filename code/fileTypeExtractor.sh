4ec4aa431f9856e6788fe9eeaa3447ea08bfc4fb
Razvan Deaconescu <razvan.deaconescu@cs.pub.ro>
2017-01-24 17:33:33 +0200

    Provide input path to file type extractor

--- code/fileTypeExtractor.sh
+++ code/fileTypeExtractor.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+if test $# -ne 1; then
+	echo "Usage: $0 /path/to/root/filesystem/" 1>&2
+	exit 1
+fi
+
+rootfs_path="$1"
+
 #the find command also has a printf option and provides much of the same data as stat
 IFS=$'\n'
 
@@ -7,16 +14,14 @@ count=0
 echoerr() { echo "$@" 1>&2; }
 echoerr $count
 
-for file in $(find . -type f);
+for file in $(find "$rootfs_path" -type f);
 do
-	adjustedFilePath=`echo $file | sed 's/^\.//'`
-	#echo $adjustedFilePath
+	adjustedFilePath=${file##"$rootfs_path"}
 	prefix=`echo file\(filePath\(\"$adjustedFilePath\"\)`
 	fileType=`file -b -p $file | sed 's/"//g' | sed 's/\`//g' | sed "s/'//g" `
 	echo $prefix,fileType\(\"$fileType\"\)\).
-        count=$((count + 1))
-        if ! (($count % 1000)); then
-                echoerr $count
-        fi
-
+	count=$((count + 1))
+	if ! (($count % 1000)); then
+		echoerr $count
+	fi
 done
