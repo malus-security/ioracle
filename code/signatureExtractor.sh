a7e4bc318a091ebb67549c96bbcf21768449f88c
Razvan Deaconescu <razvan.deaconescu@cs.pub.ro>
2017-01-26 19:23:48 +0200

    Provide rootfs argument to signature extractor

--- code/signatureExtractor.sh
+++ code/signatureExtractor.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 
+if test $# -ne 1; then
+	echo "Usage: $0 /path/to/root/filesystem/" 1>&2
+	exit 1
+fi
+
+rootfs_path="$1"
+
 #the find command also has a printf option and provides much of the same data as stat
 IFS=$'\n'
 
 echoerr() { echo "$@" 1>&2; }
 
-filename="$1"
-
 while read line; do
     #expects path to root of iOS file system as an argument.
     #todo add usage instructions as error output if argument is missing
-    filePath="$1$line"
-    #filePath="./FileSystem$line"
+    filePath="$rootfs_path$line"
 
     #this current version only outputs results for programs with com.apple as the start of their identifiers
     identifier=`codesign --display --verbose=4 $filePath 2>&1 | grep -o '^Identifier=com.apple.*' | sed 's/Identifier=//'`
