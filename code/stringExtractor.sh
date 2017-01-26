75af097f0a302a6c9cffd55c79c09da6e3e73a04
Razvan Deaconescu <razvan.deaconescu@cs.pub.ro>
2017-01-26 19:24:14 +0200

    Provide rootfs argument to string extractor

--- code/stringExtractor.sh
+++ code/stringExtractor.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 
+if test $# -ne 1; then
+	echo "Usage: $0 /path/to/root/filesystem/" 1>&2
+	exit 1
+fi
+
+rootfs_path="$1"
+
 #the find command also has a printf option and provides much of the same data as stat
 IFS=$'\n'
 
 echoerr() { echo "$@" 1>&2; }
 
-filename='appleProgramList.out'
-filelines=`cat $filename`
-
-for line in $filelines ; 
-do
+while read line; do
     #./FileSystem is hardcoded and should be changed for other systems.
     #this should really be a parameter passed in as a command line argument...
-    filePath="/home/ladeshot/FileSystem9.0.2$line"
+    filePath="$rootfs_path$line"
 
     #echo "about to process $line"
 
@@ -25,12 +28,8 @@ do
     thisSetOfStrings=`strings -n 7 $filePath | grep '[a-zA-Z0-9][a-zA-Z0-9][a-zA-Z0-9]' | grep -v '"' | grep -v '\\\\'`
 
     #echo "about to iterate through strings"
-    for stringEntry in $thisSetOfStrings ;
-    do 
+    for stringEntry in $thisSetOfStrings; do
         #echo "about to output a prolog fact"
         echo "process(filePath(\"$line\"),stringFromProgram(\"$stringEntry\"))."
     done
-
-
 done
-
