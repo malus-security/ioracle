e69fda5c4f1d02c5682fec59875d96b07a324ea6
Razvan Deaconescu <razvan.deaconescu@cs.pub.ro>
2017-02-02 17:41:24 +0200

    Provide rootfs argument to sandbox_init finding script

--- code/sandboxInitCheck.sh
+++ code/sandboxInitCheck.sh
@@ -1,33 +1,30 @@
 #!/bin/bash
 
+if test $# -ne 1; then
+	echo "Usage: $0 /path/to/root/filesystem/" 1>&2
+	exit 1
+fi
+
+rootfs_path="$1"
+
 #the find command also has a printf option and provides much of the same data as stat
 IFS=$'\n'
 
-#we needed this when using the codesign utility, but we shouldn't need it in this script.
-#echoerr() { echo "$@" 1>&2; }
+echoerr() { echo "$@" 1>&2; }
 
-filename='programWithUnknownProfiles.out'
-filelines=`cat $filename`
+while read line; do
+    #expects path to root of iOS file system as an argument.
+    #todo add usage instructions as error output if argument is missing
+    filePath="$rootfs_path$line"
 
-for line in $filelines ; 
-do
-    #FileSystem path is hardcoded and should be changed for other systems.
-    filePath="/home/ladeshot/FileSystem9.0.2$line"
-
-    #result=`strings $filePath | grep 'sandbox'`
-    #result=`strings $filePath | grep 'sandbox_init'`
-    result=`strings $filePath | grep 'sandbox_init\|apply_container'`
-    #result=`strings $filePath | grep 'apply_container'`
-    #result=`strings $filePath | grep '<key>com.apple.private.security.no-sandbox</key>'`
-    #result=`grep 'sandbox_init' $filePath`
-    #identifier=`codesign --display --verbose=4 $filePath 2>&1 | grep -o '^Identifier=com.apple.*' | sed 's/Identifier=//'`
-
-    #-z checks to see if the string is empty.
-    #no identifier should indicate that the executable had no signature
-    if [ ! -z "$result" ]; then
-      #echo "process(filepath(\"$line\"),result(\"$result\"))."
-      echo $line
+    strings "$filePath" | grep 'apply_container' > /dev/null 2>&1
+    if test $? -eq 0; then
+        echo 'usesSandbox(processPath("'"$line"'"),profile("container"),mechanism("apply_container")).'
+        continue    # Do not check for sandbox_init() fi apply_container() is already there.
     fi
-
+    strings "$filePath" | grep 'sandbox_init' > /dev/null 2>&1
+    if test $? -eq 0; then
+        echo 'usesSandbox(processPath("'"$line"'"),profile("unknown"),mechanism("sandbox_init")).'
+    fi
 done
 
