73210ace0122b50fd7493aa3075382193b738275
Razvan Deaconescu <razvan.deaconescu@cs.pub.ro>
2017-02-10 17:46:38 +0200

    Add printing queries for process mappings

--- queries/get_process_user_group_mappings.pl
+++ queries/get_process_user_group_mappings.pl
@@ -1,14 +1,59 @@
-process_profile :- [process_mapping_queries, iPhoneSE_13F69_iOS932_process_ownership, iPhoneSE_13F69_iOS932_sandbox_profile_mappings, iPhoneSE_13F69_iOS932_apple_executable_files],
+apple_process_sandbox :- [process_mapping_queries, iPhoneSE_13F69_iOS932_process_ownership, iPhoneSE_13F69_iOS932_sandbox_profile_mappings, iPhoneSE_13F69_iOS932_apple_executable_files],
    findall((Process,SandboxProfile,SandboxMechanism), mapProcessSandboxProfile(Process,SandboxProfile,SandboxMechanism), L),
    write(L),
    halt.
 
-process_permission :- [process_mapping_queries, iPhoneSE_13F69_iOS932_users, iPhoneSE_13F69_iOS932_groups, iPhoneSE_13F69_iOS932_process_ownership, iPhoneSE_13F69_iOS932_apple_executable_files, file_metadata],
+print_apple_process_sandbox :- [process_mapping_queries, iPhoneSE_13F69_iOS932_process_ownership, iPhoneSE_13F69_iOS932_sandbox_profile_mappings, iPhoneSE_13F69_iOS932_apple_executable_files],
+   findall((Process,SandboxProfile,SandboxMechanism), mapProcessSandboxProfile(Process,SandboxProfile,SandboxMechanism), L),
+   member((P,S,M),L),
+   write("mapAppleProcessSandbox(process(\""),write(P),write("\"),profile(\""),write(S),write("\"),mechanism(\""),write(M),writeln("\")."),
+   fail.
+
+exec_apple_exec_sandbox :- [process_mapping_queries, iPhoneSE_13F69_iOS932_sandbox_profile_mappings, iPhoneSE_13F69_iOS932_apple_executable_files],
+   findall((Executable,SandboxProfile,SandboxMechanism), mapAppleExecSandboxProfile(Executable,SandboxProfile,SandboxMechanism), L),
+   write(L),
+   halt.
+
+print_apple_exec_sandbox :- [process_mapping_queries, iPhoneSE_13F69_iOS932_sandbox_profile_mappings, iPhoneSE_13F69_iOS932_apple_executable_files],
+   findall((Executable,SandboxProfile,SandboxMechanism), mapAppleExecSandboxProfile(Executable,SandboxProfile,SandboxMechanism), L),
+   member((E,P,M),L),
+   write("mapAppleExecSandbox(filePath(\""),write(E),write("\"),profile(\""),write(P),write("\"),mechanism(\""),write(M),writeln("\")."),
+   fail.
+
+apple_process_permission :- [process_mapping_queries, iPhoneSE_13F69_iOS932_users, iPhoneSE_13F69_iOS932_groups, iPhoneSE_13F69_iOS932_process_ownership, iPhoneSE_13F69_iOS932_apple_executable_files, file_metadata],
    findall((Process,FilePath,Permission), mapProcessPermissions(Process,FilePath,Permission), L),
    write(L),
    halt.
+
+print_apple_process_permission :- [process_mapping_queries, iPhoneSE_13F69_iOS932_users, iPhoneSE_13F69_iOS932_groups, iPhoneSE_13F69_iOS932_process_ownership, iPhoneSE_13F69_iOS932_apple_executable_files, file_metadata],
+   findall((Process,FilePath,Permission), mapProcessPermissions(Process,FilePath,Permission), L),
+   member((P,F,A),L),
+   write("mapAppleProcessFilePermission(process(\""),write(P),write("\"),filePath(\""),write(F),write("\"),permission(\""),write(A),writeln("\")."),
+   fail.
 
 process_file :- [process_mapping_queries, iPhoneSE_13F69_iOS932_users, iPhoneSE_13F69_iOS932_groups, iPhoneSE_13F69_iOS932_process_ownership, iPhoneSE_13F69_iOS932_apple_executable_files],
    findall((X,Y,Z), mapProcessUserGroup(X,Y,Z), L),
    write(L),
    halt.
+
+apple_process_no_sandbox :- [process_mapping_queries, iPhoneSE_13F69_iOS932_process_ownership, iPhoneSE_13F69_iOS932_sandbox_profile_mappings, iPhoneSE_13F69_iOS932_apple_executable_files],
+   findall(Process, (processAppleNoSandboxProfile(Process)), L),
+   write(L),
+   halt.
+
+print_apple_process_no_sandbox :- [process_mapping_queries, iPhoneSE_13F69_iOS932_process_ownership, iPhoneSE_13F69_iOS932_sandbox_profile_mappings, iPhoneSE_13F69_iOS932_apple_executable_files],
+   findall(Process, (processAppleNoSandboxProfile(Process)), L),
+   member(P,L),
+   write("noSandbox(process(\""),write(P),writeln("\"))."),
+   fail.
+
+apple_exec_no_sandbox :- [process_mapping_queries, iPhoneSE_13F69_iOS932_sandbox_profile_mappings, iPhoneSE_13F69_iOS932_apple_executable_files],
+   findall(filePath, (appleExecNoSandboxProfile(filePath)), L),
+   write(L),
+   halt.
+
+print_apple_exec_no_sandbox :- [process_mapping_queries, iPhoneSE_13F69_iOS932_sandbox_profile_mappings, iPhoneSE_13F69_iOS932_apple_executable_files],
+   findall(Executable, (appleExecNoSandboxProfile(Executable)), L),
+   member(E,L),
+   write("noSandbox(filePath(\""),write(E),writeln("\"))."),
+   fail.
--- queries/process_mapping_queries.pl
+++ queries/process_mapping_queries.pl
@@ -8,16 +8,35 @@ mapProcessUserGroup(Process,UserName,GroupName) :-
 	user(userName(UserName),_,userID(UserId),_,_,_,_),
 	group(groupName(GroupName),_,id(GroupId),_).
 
-mapProcessPermissions(Process, FilePath, Permission) :-
+uniqueFile(FilePath) :-
+	findall(F, file(filepath(F),_), L),
+	list_to_set(L, S),
+	member(FilePath, S).
+
+mapProcessPermissions(Process,FilePath,Permission) :-
 	mapProcessUserGroup(Process,UserName,GroupName),
-	file(FilePath, permissionBits(P)),
-	file(FilePath, ownerUserName(U)),
-	file(FilePath, ownerGroupName(G)),
-	( U = UserName, UserPermission is mod(div(P, 100), 10); UserPermission is mod(P,10)),
-	( G = GroupName, GroupPermission is mod(div(P, 10),10); GroupPermission is mod(P,10)),
-	( UserPermission >= GroupPermission, Permission is UserPermission; Permission is GroupPermission).
+	uniqueFile(FilePath),
+	file(filepath(FilePath), permissionBits(P)),
+	file(filepath(FilePath), ownerUserName(U)),
+	file(filepath(FilePath), ownerGroupName(G)),
+	(( U = UserName ) -> UserPermission is mod(div(P, 100), 10); UserPermission is mod(P,10)),
+	(( G = GroupName ) -> GroupPermission is mod(div(P, 10),10); GroupPermission is mod(P,10)),
+	(( UserPermission >= GroupPermission) -> Permission is UserPermission; Permission is GroupPermission).
 
 mapProcessSandboxProfile(Process,SandboxProfile,SandboxMechanism) :-
 	mapProcessAppleFiles(Process),
 	processOwnership(_,_,comm(Process)),
 	usesSandbox(processPath(Process),profile(SandboxProfile),mechanism(SandboxMechanism)).
+
+mapAppleExecSandboxProfile(Executable,SandboxProfile,SandboxMechanism) :-
+	processSignature(filePath(Executable),_),
+	usesSandbox(processPath(Executable),profile(SandboxProfile),mechanism(SandboxMechanism)).
+
+processAppleNoSandboxProfile(Process) :-
+	processOwnership(_,_,comm(Process)),
+	processSignature(filePath(Process),_),
+	not(usesSandbox(processPath(Process),_,_)).
+
+appleExecNoSandboxProfile(Executable) :-
+	processSignature(filePath(Executable),_),
+	not(usesSandbox(processPath(Executable),_,_)).
