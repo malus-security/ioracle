c8f7b8d5761f49ac277e97449ec65b3201b57c63
alecdeshotels <ladeshot@ncsu.edu>
2017-02-15 17:18:54 -0500

    added more descriptive output for phases of data extraction in automation script. Fixed a bug in stringExtractor.sh that caused it to freeze (maybe ran out of memory?). Added the iOS file path used to determine if a process would get assigned the container policy.

--- automatedDataExtraction/dataExtractorForExtractedFileSystem.sh
+++ automatedDataExtraction/dataExtractorForExtractedFileSystem.sh
@@ -9,39 +9,51 @@ fi
 extractionDirectory="$1"
 rm -rf ./temporaryFiles
 mkdir ./temporaryFiles
+mkdir $extractionDirectory/prologFacts
 
+echo 'getting file types'
 ##get file types from the file system extracted to the local system
 ./scriptsToAutomate/fileTypeExtractor.sh $extractionDirectory/fileSystem > $extractionDirectory/prologFacts/file_types.pl
 
+echo 'getting user data'
 #extract data about users from etc
 ./scriptsToAutomate/userFactExtractor.sh $extractionDirectory/fileSystem > $extractionDirectory/prologFacts/users.pl
 
+echo 'getting group data'
 #extract data about groups from etc
 ./scriptsToAutomate/groupFactExtractor.sh $extractionDirectory/fileSystem > $extractionDirectory/prologFacts/groups.pl
 
+echo 'getting file paths of Mach-O executables'
 cat $extractionDirectory/prologFacts/file_types.pl ./scriptsToAutomate/queries.pl > ./temporaryFiles/relevantFacts.pl
 ./scriptsToAutomate/runProlog.sh justPaths $extractionDirectory/fileSystem > ./temporaryFiles/filePaths.out
 rm ./temporaryFiles/relevantFacts.pl
 
+echo 'getting signatures of Apple-Signed Mach-O executables'
 ./scriptsToAutomate/signatureExtractor.sh $extractionDirectory/fileSystem < ./temporaryFiles/filePaths.out > $extractionDirectory/prologFacts/apple_executable_files_signatures.pl
 
+echo 'getting file paths for Apple-Signed Mach-O executables'
 #generate a list of file paths to Apple-signed mach-o executable files
 cat $extractionDirectory/prologFacts/apple_executable_files_signatures.pl ./scriptsToAutomate/queries.pl > ./temporaryFiles/relevantFacts.pl
 ./scriptsToAutomate/runProlog.sh justApplePaths $extractionDirectory/fileSystem > ./temporaryFiles/applefilePaths.out
 rm ./temporaryFiles/relevantFacts.pl
 
+echo 'getting entitlements for Apple-Signed Mach-O executables'
 #extract entitlements from programs listed in the input 
 ./scriptsToAutomate/entitlementExtractor.sh $extractionDirectory/fileSystem < ./temporaryFiles/applefilePaths.out > $extractionDirectory/prologFacts/apple_executable_files_entitlements.pl
 
+echo 'getting strings for Apple-Signed Mach-O executables'
 ./scriptsToAutomate/stringExtractor.sh $extractionDirectory/fileSystem < ./temporaryFiles/applefilePaths.out > $extractionDirectory/prologFacts/apple_executable_files_strings.pl
 
+echo 'getting symbols for Apple-Signed Mach-O executables'
 ./scriptsToAutomate/symbolExtractor.sh $extractionDirectory/fileSystem < ./temporaryFiles/applefilePaths.out > $extractionDirectory/prologFacts/apple_executable_files_symbols.pl
 
+echo 'getting sandbox profile assignments based on entitlements and file paths'
 #TODO Why am I listing the file system as an argument to runProlog.sh?
 cat $extractionDirectory/prologFacts/apple_executable_files_signatures.pl $extractionDirectory/prologFacts/apple_executable_files_entitlements.pl ./scriptsToAutomate/queries.pl > ./temporaryFiles/relevantFacts.pl
 ./scriptsToAutomate/runProlog.sh getProfilesFromEntitlementsAndPaths > ./temporaryFiles/profileAssignmentFromEntAndPath.pl
 rm ./temporaryFiles/relevantFacts.pl
 
+echo 'getting file paths to processes that assign sandboxes to themselves.'
 cat $extractionDirectory/prologFacts/apple_executable_files_symbols.pl ./scriptsToAutomate/queries.pl > ./temporaryFiles/relevantFacts.pl
 ./scriptsToAutomate/runProlog.sh getSelfAssigningProcessesWithSymbols > ./temporaryFiles/pathsToSelfAssigners.out
 rm ./temporaryFiles/relevantFacts.pl
@@ -54,6 +66,7 @@ rm ./temporaryFiles/relevantFacts.pl
 
 ./scriptsToAutomate/mapIdaScriptToTargets.sh ./temporaryFiles/hashedPathToFilePathMapping.csv ./scriptsToAutomate/strider.py ./temporaryFiles/ ./temporaryFiles/applyContainer.out ./configurationFiles/applyContainer.config
 
+echo 'consolidating output of IDA analysis on sandbox self assigners with assignments based on entitlements and file paths.'
 cat ./temporaryFiles/applyContainer.out temporaryFiles/sandboxInit.out temporaryFiles/sandboxInitWithParameters.out > ./temporaryFiles/selfApplySandbox.pl
 
 cat ./temporaryFiles/selfApplySandbox.pl ./scriptsToAutomate/queries.pl > ./temporaryFiles/relevantFacts.pl
