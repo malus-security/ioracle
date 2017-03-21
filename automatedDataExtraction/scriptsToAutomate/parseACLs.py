ed7e16537af11d02bf709ec909a80ee0b7dd2322
alecdeshotels <ladeshot@ncsu>
2017-03-21 16:49:40 -0400

    created a python script that converts output of the getfacl program into prolog facts that represent posix ACLs for files on iOS device..

---
+++ automatedDataExtraction/scriptsToAutomate/parseACLs.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+import sys
+
+inputPath = sys.argv[1]+"/aclOutput.out"
+f = open(inputPath, 'r')
+fileResults = f.read().strip().split("\n/")
+
+for l in fileResults:
+  lines = l.split('\n')
+  object = lines[0]
+  aclLines = lines[1:]
+  count = 0
+  for a in aclLines:
+    colonSplit = a.split(':')
+    subjectType = colonSplit[0]
+    spaceSplit = colonSplit[1].split(' ')
+    subject = spaceSplit[0]
+
+    inheritance = ""
+    decision = ""
+    operations = ""
+    
+    if len(spaceSplit) == 3:
+      inheritance = "notInherited"
+      decision = spaceSplit[1]
+      operations = spaceSplit[2]
+    elif len(spaceSplit) == 4:
+      inheritance = spaceSplit[1]
+      decision = spaceSplit[2]
+      operations = spaceSplit[3]
+    else:
+      print "ERROR: unexpected list size:"
+      print spaceSplit
+      
+    #print 'fileACL(filePath('+filePath+'),',
+    print 'fileACL(',
+    print 'ruleNumber(' + str(count) + "),",
+    print 'object("' + object + '"),',
+    print 'subjectType(' + subjectType + "),",
+    print 'subject(' + subject + '),',
+    print 'inheritance(' + inheritance +"),",
+    print 'decision(' + decision + "),",
+
+    print 'operations([',
+    ops = operations.split(',')
+    for i in range(len(ops)):
+      if i < (len(ops)-1):
+	print '"' + ops[i] + '",',
+      else:
+	print '"' + ops[i] + '"',
+    print '])).'
+
+    count = count + 1
+
