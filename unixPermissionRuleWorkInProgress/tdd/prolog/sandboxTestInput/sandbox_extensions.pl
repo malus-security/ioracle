36a1e9aaabe938c33a2be14e06cdb4bb7c560580
alecdeshotels <ladeshot@ncsu.edu>
2017-04-21 21:54:41 -0400

    Changed how we represent sandbox extensions in tests to reflect my new understanding of them. Added tests and functionality for file type sandbox extensions. I still need to implement a way to handle mach type sandbox extensions.

--- unixPermissionRuleWorkInProgress/tdd/prolog/sandboxTestInput/sandbox_extensions.pl
+++ unixPermissionRuleWorkInProgress/tdd/prolog/sandboxTestInput/sandbox_extensions.pl
@@ -1,4 +1,9 @@
-sandboxExtension(filePath("/mobile/process"),extension("commonExtension")).
-sandboxExtension(filePath("/mobile/process"),extension("mobileExtension")).
-sandboxExtension(filePath("/root/process"),extension("commonExtension")).
-sandboxExtension(filePath("/root/process"),extension("rootExtension")).
+%sandboxExtension(filePath("/mobile/process"),extension("commonExtension")).
+%sandboxExtension(filePath("/mobile/process"),extension("mobileExtension")).
+%sandboxExtension(filePath("/root/process"),extension("commonExtension")).
+%sandboxExtension(filePath("/root/process"),extension("rootExtension")).
+
+sandboxExtension(process("/mobile/process"),extension(class("file.read.class"),type("file"),value("/wildcard/readable/file"))).
+sandboxExtension(process("/mobile/process"),extension(class("file.write.class"),type("file"),value("/wildcard/writable/file"))).
+sandboxExtension(process("/mobile/process"),extension(class("root.class"),type("file"),value("/"))).
+sandboxExtension(process("/mobile/process"),extension(class("mach.class"),type("mach"),value("some.mach.service"))).
