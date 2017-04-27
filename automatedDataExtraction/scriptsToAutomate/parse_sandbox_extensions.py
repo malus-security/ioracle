577319c899af75ffe8f7fbcb4613ad886441b651
alecdeshotels <ladeshot@ncsu>
2017-04-27 16:54:22 -0400

    starting a python script to convert output of sbtool into prolog facts.

---
+++ automatedDataExtraction/scriptsToAutomate/parse_sandbox_extensions.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+import sys
+
+inputPath = sys.argv[1]
+f = open(inputPath, 'r')
+processResults = f.read().strip().split("\n")
+
+for processLine in processResults:
+  print processLine
+  #extract the PID
+  #cut out anything before extensions start
+  #split the extension classess apart from each other
+  #extract the class name
+  #cut out anything before values
+  #split the values within each class
+  #extract the value type and value string for each value (should be ok to ignore other fields for now).
+
+"""
+  processLine = l.split('\n')
+  object = lines[0]
+  #if the object doesn't begin with a / then add one in
+  if object[0] != "/":
+    object = "/" + object
+  aclLines = lines[1:]
+  count = 0
+  for a in aclLines:
+    colonSplit = a.split(':')
+    subjectType = colonSplit[0]
+    spaceSplit = colonSplit[1].split(' ')
+    subject = spaceSplit[0]
+
+    inheritance = ""
+    decision = ""
+    operations = ""
+    
+    if len(spaceSplit) == 3:
+      inheritance = "notInherited"
+      decision = spaceSplit[1]
+      operations = spaceSplit[2]
+    elif len(spaceSplit) == 4:
+      inheritance = spaceSplit[1]
+      decision = spaceSplit[2]
+      operations = spaceSplit[3]
+    else:
+      print "ERROR: unexpected list size:"
+      print spaceSplit
+      
+    #print 'fileACL(filePath('+filePath+'),',
+    print 'fileACL(',
+    print 'ruleNumber(' + str(count) + "),",
+    print 'object("' + object + '"),',
+    print 'subjectType(' + subjectType + "),",
+    print 'subject(' + subject + '),',
+    print 'inheritance(' + inheritance +"),",
+    print 'decision(' + decision + "),",
+
+    print 'operations([',
+    ops = operations.split(',')
+    for i in range(len(ops)):
+      if i < (len(ops)-1):
+	print '"' + ops[i] + '",',
+      else:
+	print '"' + ops[i] + '"',
+    print '])).'
+
+    count = count + 1
+"""
