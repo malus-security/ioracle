14993b9da03876b49bc4589e19aafbfbc5378dd8
alecdeshotels <ladeshot@ncsu.edu>
2017-05-02 09:20:25 -0400

    improved output of extension parser and added comments with suggestions for mapping pid numbers to process executables.

--- automatedDataExtraction/scriptsToAutomate/parse_sandbox_extensions.py
+++ automatedDataExtraction/scriptsToAutomate/parse_sandbox_extensions.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python
+
+#TODO I am assuming that extensions without a class type are generics and labelling them that way in the facts.
+#we will need to modify this if I'm wrong about how to handle this type of extension.
+#when there is more time, we should look into these by figuring out who issues them and analyzing the issuers with IDA.
+#for now, we don't really care though since the only extensions we're worried about are for files and mach services.
+
 import sys
 import re
 
@@ -15,87 +21,53 @@ for processLine in processResults:
   extensions_string = process_matches.group(2)
   #split the extension classess apart from each other
   extensions_list = extensions_string.split("extensions ")
-  print "processID: "+process_id
+  #print "processID: "+process_id
   for extension_class in extensions_list:
     if "class" in  extension_class:
-      print extension_class
+      #print extension_class
       class_pattern = re.compile("^.*?class\:\ ([^)]+)\)\ {(.*)}.*$")
       class_matches = re.match(class_pattern,extension_class)
       #extract the class name
       class_string = class_matches.group(1)
       #cut out anything before values
       class_values_string = class_matches.group(2)
-      print "class_string: "+class_string
-      print "class_values_string: "+class_values_string
+      #print "class_string: "+class_string
+      #print "class_values_string: "+class_values_string
 
-      #TODO we might have trouble when there are no values for the extension class.
-      #We should be able to model these somehow in the prolog facts though.
-      #Maybe the values could be in a prolog list, and if there aren't any we just make the list empty
-      #sandbox_extension(class(),values[value(value_type(),value_string()),value(value_type(),value_string())]).
-      #sandbox_extension(class(),values[]).
-  
       #split the values within each class
       class_values_list = class_values_string.split("flags=")
 
       for class_value in class_values_list:
 	if ":" in class_value:
-	  print "class_value: "+class_value
+	  #print "class_value: "+class_value
 	  value_pattern = re.compile("^.*?([^\ ]+):\ (.*?)[;\ ]")
 	  value_matches = re.match(value_pattern,class_value)
 	  #extract the value type and value string for each value (should be ok to ignore other fields for now).
 	  type_string = value_matches.group(1)
 	  value_string = value_matches.group(2)
-	  print "type_string: "+type_string
-	  print "value_string: "+value_string
+	  #print "type_string: "+type_string
+	  #print "value_string: "+value_string
+
+	  print 'sandbox_extension(',
+	  #TODO the query is expecting the file path of the process, not the process id number.
+	  #this is going to be tricky, but it's a problem we need to solve...
+	  #we should be able to output a separate file that maps pid numbers to process file paths.
+	  #then we can use logic in this script to match them up before outputting these facts.
+	  #if we assume the pids will not wrap around, then our lived become much easier.
+	  #we can output a warning if a wrap around happens, but I don't expect it to be an issue during our tests.
+	  #It should only be an issue if we try to harvest logs across a reboot.
+	  print 'process_id_number("' + str(process_id) + '"),',
+	  print 'extension(',
+	  print 'class("' + class_string+ '"),',
+	  print 'type("' + type_string + '"),',
+	  print 'value("' + value_string +'"))).'
+
+	else:
+	  print 'sandbox_extension(',
+	  print 'process_id_number("' + str(process_id) + '"),',
+	  print 'extension(',
+	  print 'class("' + class_string+ '"),',
+	  print 'type("generic"),',
+	  print 'value(""))).'
 
 
-"""
-  processLine = l.split('\n')
-  object = lines[0]
-  #if the object doesn't begin with a / then add one in
-  if object[0] != "/":
-    object = "/" + object
-  aclLines = lines[1:]
-  count = 0
-  for a in aclLines:
-    colonSplit = a.split(':')
-    subjectType = colonSplit[0]
-    spaceSplit = colonSplit[1].split(' ')
-    subject = spaceSplit[0]
-
-    inheritance = ""
-    decision = ""
-    operations = ""
-    
-    if len(spaceSplit) == 3:
-      inheritance = "notInherited"
-      decision = spaceSplit[1]
-      operations = spaceSplit[2]
-    elif len(spaceSplit) == 4:
-      inheritance = spaceSplit[1]
-      decision = spaceSplit[2]
-      operations = spaceSplit[3]
-    else:
-      print "ERROR: unexpected list size:"
-      print spaceSplit
-      
-    #print 'fileACL(filePath('+filePath+'),',
-    print 'fileACL(',
-    print 'ruleNumber(' + str(count) + "),",
-    print 'object("' + object + '"),',
-    print 'subjectType(' + subjectType + "),",
-    print 'subject(' + subject + '),',
-    print 'inheritance(' + inheritance +"),",
-    print 'decision(' + decision + "),",
-
-    print 'operations([',
-    ops = operations.split(',')
-    for i in range(len(ops)):
-      if i < (len(ops)-1):
-	print '"' + ops[i] + '",',
-      else:
-	print '"' + ops[i] + '"',
-    print '])).'
-
-    count = count + 1
-"""
