2ade4ec8b0089fdb8cecd16b4cf80b88a3101836
Costin Carabas <costin.carabas@nxp.com>
2017-05-08 23:31:40 +0100

    dynamic analysis of file access
    code/automatedScenarios
    
    Signed-off-by: Costin Carabas <costin.carabas@nxp.com>

---
+++ code/automatedScenarios/powerDown.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+##############################################################################
+# You need an app called "activator" from cydia in order to run the script   #
+##############################################################################
+
+source ../utils
+
+if test $# -ne 4; then
+	echo "Usage: $0 userOniOSDevice hostOfiOSdevice portForiOSDevice directoryForOutput" 1>&2
+	echo "Example: $0 root localhost 2270 directoryForOutput" 1>&2
+	echo "WARNING: This script will sudo rm -rf the directory at the path of directoryForOutput" 1>&2
+	exit 1
+fi
+
+user="$1"
+host="$2"
+port="$3"
+directoryForOutput="$4"
+filemonPath="/var/root/filemon"
+
+ssh -p $port -n $user@$host "test -e $filemonPath"
+if [ ! $? -eq 0 ]; then
+    echo "Filemon not found! Please download it and retry."
+    exit 0
+fi
+
+rm -rf ./$directoryForOutput
+mkdir $directoryForOutput
+
+ssh -p $port $user@$host "killall filemon"
+ssh -p $port $user@$host "rm /tmp/filemonRawOutput.txt"
+ssh -p $port -n $user@$host "$filemonPath > /tmp/filemonRawOutput.txt & reboot"
+
+while ping -c 1 $host &>/dev/null; do :; done
+echo "Waiting for a connection... Please re-jailbreak (iOS 9/10) or wait for reboot (iOS < 9)..."
+while ! ping -c 1 $host &>/dev/null; do :; done
+
+echo Generating the facts...
+time ssh -p $port $user@$host 'bash -s' < ../logFileAccessObservations.sh > $directoryForOutput/fileAccessObservations.pl
