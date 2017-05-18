22734d49c11968417d55a247013eae59400ad09d
alecdeshotels <ladeshot@ncsu.edu>
2017-05-18 15:14:00 -0400

    removing prolog rules related to resolving symlinks since we did everything in python instead

--- rules/helper_rules.pl
+++ rules/helper_rules.pl
@@ -23,35 +23,4 @@ simplified_high_paths(Process,File):-
     (fileAccessObservation(process(Process),sourceFile(_),destinationFile(File),operation(_)))
   ).
 
-spit_out_paths_for_dynamic:-
-  (
-    (fileAccessObservation(process(Path),sourceFile(_),destinationFile(_),operation(_)))
-    ;
-    (fileAccessObservation(process(_),sourceFile(Path),destinationFile(_),operation(_)))
-    ;
-    (fileAccessObservation(process(_),sourceFile(_),destinationFile(Path),operation(_)))
-    ;
-    (processOwnership(uid(_),gid(_),comm(Path)))
-    ;
-    (sandbox_extension(process(Path),extension(class(_),type(_),value(_))))
-    ;
-    (sandbox_extension(process(_),extension(class(_),type("file"),value(Path))))
-  ),
-  writeln(Path),
-  fail.
 
-%start simple with just fileAccessObservation processes, then make this more general.
-fix_process_strings:-
-  (fileAccessObservation(process(Process),sourceFile(_),destinationFile(_),operation(_))),
-  symlink_machine(Process,Result),
-  writeln(Process),
-  writeln(Result).
-
-%base case
-symlink_machine("/","/").
-  %don't do anything if root directory, just return same thing as result.
-
-symlink_machine(FilePath,Result):-
-  %is the current file a symlink?
-  fail.
-  %consider parent
