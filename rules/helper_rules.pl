08a9e6cb6a0150cf026bd55cfdb03a9355b69a4e
alecdeshotels <ladeshot@ncsu.edu>
2017-05-25 13:15:55 -0400

    adding some high level prolog rules that should help us find file path integrity violations. These rules are similar to the ones described in our paper (e.g., find high integrity processes).

--- rules/helper_rules.pl
+++ rules/helper_rules.pl
@@ -7,20 +7,71 @@ observed_high_integrity_process(Process):-
   high_integrity_process(Process),
   fileAccessObservation(process(Process),sourceFile(_),destinationFile(_),operation(_)).
 
-high_integrity_paths(Process,File):-
+high_integrity_paths(File):-
   high_integrity_process(Process),
   (
     (fileAccessObservation(process(Process),sourceFile(File),destinationFile(_),operation(_)))
     ;
-    (fileAccessObservation(process(Process),sourceFile(_),destinationFile(File),operation(_)))
+    (
+      fileAccessObservation(process(Process),sourceFile(_),destinationFile(File),operation(_)),
+      not(File == "No destination")
+    )
   ).
 
-simplified_high_paths(Process,File):-
-  not(usesSandbox(processPath(Process),profile(_),mechanism(_))),
+%do we care whether the file was the source or destination of the operation?
+high_integrity_access(Process,File,Operation):-
+  high_integrity_process(Process),
   (
-    (fileAccessObservation(process(Process),sourceFile(File),destinationFile(_),operation(_)))
+    (fileAccessObservation(process(Process),sourceFile(File),destinationFile(_),operation(Operation)))
     ;
-    (fileAccessObservation(process(Process),sourceFile(_),destinationFile(File),operation(_)))
+    (
+      fileAccessObservation(process(Process),sourceFile(_),destinationFile(File),operation(Operation)),
+      not(File == "No destination")
+    )
   ).
+
+
+integrity_violations_sand:-
+  %fileAccessObservation(process(Proc_obs),sourceFile(File),destinationFile(D_ops),operation(Op_obs)),
+  high_integrity_access(AccessProcess,File,AccessOperation),
+  usesSandbox(processPath(Process),profile(Profile),mechanism(_)),
+  profileDefault(profile("AdSheet"),decision("deny")),
+  getAttributes(process(Process),entitlements(Ent),extensions(Ext),user(User),home(Home),profile(Profile)),
+  %Process=="/usr/libexec/afcd",
+  relevantRule(entitlements(Ent),extensions(Ext),home(Home),profile(Profile),operation("file-writeSTAR"),subject(file(File)),decision(Decision),filters(Filters)),
+  write("High Integrity Process		:"), writeln(AccessProcess),
+  write("Integrity Violation Type 	:"), writeln(AccessOperation),
+  write("Dangerous File Path		:"), writeln(File),
+  write("Low Integrity Process		:"), writeln(Process),
+  write("Filters Allowing Access	:"), writeln(Filters),
+  writeln(""),
+  fail.
+
+integrity_violations_unix:-
+  %fileAccessObservation(process(Proc_obs),sourceFile(File),destinationFile(D_ops),operation(Op_obs)),
+  high_integrity_access(AccessProcess,File,AccessOperation),
+  unixAllow(puid("mobile"),pgid("mobile"),coarseOp("write"),file(File)),
+  write("High Integrity Process		:"), writeln(AccessProcess),
+  write("Integrity Violation Type 	:"), writeln(AccessOperation),
+  write("Dangerous File Path		:"), writeln(File),
+  write("Low Integrity Process		:"), writeln(Process),
+  writeln(""),
+  fail.
+
+integrity_violations_both:-
+  %fileAccessObservation(process(Proc_obs),sourceFile(File),destinationFile(D_ops),operation(Op_obs)),
+  high_integrity_access(AccessProcess,File,AccessOperation),
+  unixAllow(puid("mobile"),pgid("mobile"),coarseOp("write"),file(File)),
+  usesSandbox(processPath(Process),profile(Profile),mechanism(_)),
+  profileDefault(profile("AdSheet"),decision("deny")),
+  getAttributes(process(Process),entitlements(Ent),extensions(Ext),user(User),home(Home),profile(Profile)),
+  %Process=="/usr/libexec/afcd",
+  relevantRule(entitlements(Ent),extensions(Ext),home(Home),profile(Profile),operation("file-writeSTAR"),subject(file(File)),decision(Decision),filters(Filters)),
+  write("integrity_violation( high_process(\""), 	write(AccessProcess),
+  write("\"), operation(\""), 				write(AccessOperation),
+  write("\"), file_path(\""), 				write(File),
+  write("\"), low_process(\""), 			write(Process),
+  writeln("\"))."),
+  fail.
 
 
