070b88c73c67608914f8559fa3c1231712aba99d
alecdeshotels <ladeshot@ncsu.edu>
2017-05-26 15:46:41 -0400

    documented sandscout related scripts and added README for these scripts. Renamed the file that does the actual sbpl to prolog compilation.

--- allProfileSandScout/processAllProfiles.sh
+++ allProfileSandScout/processAllProfiles.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-#FILES=/Users/luke/oracle/allProfileSandScout/allProfileSBPL/*
 
 #usage details
 if test $# -ne 3; then
@@ -8,15 +7,23 @@ if test $# -ne 3; then
   exit 1
 fi
 
+#the * gets all the file in the directory into a list
 FILES=$1/*
+#we will have a file of Prolog facts for each SBPL input file.
 outputDir=$2
+#we will consolidate all of the prolog facts into one file to easily integrate it into the other facts for iOracle.
 consolidatedFacts=$3
+
 mkdir $outputDir
 for f in $FILES
 do
   echo "Processing $f file..."
-  fileName=`echo $f | sed 's;^.*/;;g' | sed 's;\.sb$;;'`
-  ./smartPly.py $f $fileName > $outputDir/$fileName.pl
+  #we need to figure out the profile name so we can list it in the prolog facts
+  #we can get the profile name by removing the parent directories and the .sb ending on the sbpl files
+  profileName=`echo $f | sed 's;^.*/;;g' | sed 's;\.sb$;;'`
+  #this is the compiler of SandScout, and it uses lex and yacc for python to compile sbpl into prolog facts.
+  ./sandscout_compiler.py $f $profileName > $outputDir/$profileName.pl
 done
 
+#consolidate all output files into one convenient file for integrating into iOracle fact collection
 cat $outputDir/* > $consolidatedFacts
