fb5d1a33a975e9d4c38a7ada3ee2a2d5b06aa55d
alecdeshotels <ladeshot@ncsu.edu>
2017-06-22 14:53:30 -0400

    modifying metaDataExtractor to get uid number and gid number so that it will match the data acquirable from firmware images.

--- automatedDataExtraction/scriptsToAutomate/metaDataExtractor.sh
+++ automatedDataExtraction/scriptsToAutomate/metaDataExtractor.sh
@@ -8,4 +8,4 @@ fi
 rootfs_path="$1/"
 rootfs_path=${rootfs_path//\/\//\/}
 
-find "$rootfs_path" -printf 'fileSize(size(%s),filePath("%p")).\nfileOwnerGroupName(ownerGroupName("%g"),filePath("%p")).\nfileLastModification(lastModification(%T@),filePath("%p")).\nfileInode(inode(%i),filePath("%p")).\nfileSymLink(symLinkObject("%l"),filePath("%p")).\nfilePermissionBits(permissionBits(%m),filePath("%p")).\nfileNumHardLinks(numHardLinks(%n),filePath("%p")).\nfileOwnerUserName(ownerUserName("%u"),filePath("%p")).\nfileType(type("%y"),filePath("%p")).\n'
+find "$rootfs_path" -printf 'fileSize(size(%s),filePath("%p")).\nfileOwnerGroupNumber(ownerGroupNumber("%G"),filePath("%p")).\nfileLastModification(lastModification(%T@),filePath("%p")).\nfileInode(inode(%i),filePath("%p")).\nfileSymLink(symLinkObject("%l"),filePath("%p")).\nfilePermissionBits(permissionBits(%m),filePath("%p")).\nfileNumHardLinks(numHardLinks(%n),filePath("%p")).\nfileOwnerUserNumber(ownerUserNumber("%U"),filePath("%p")).\nfileType(type("%y"),filePath("%p")).\n'
