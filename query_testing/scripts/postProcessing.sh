ba6e53b0cb398d8619e078e104c9172fb175d14a
alecdeshotels <ladeshot@ncsu.edu>
2017-07-12 15:52:14 -0400

    I modified the post processor to compute the parent directories for the file paths identified in dynamic analysis and with the backtracer. This allows us to properly identify the chown vulnerability for evasion 7.

--- query_testing/scripts/postProcessing.sh
+++ query_testing/scripts/postProcessing.sh
@@ -10,8 +10,15 @@ inputDirectory=$1
 #this seems to be the output file for the facts and rules used in post processing for file_metadata
 plPost="$inputDirectory/temporaryFiles/postProcessing.pl"
 
-cat $inputDirectory/prologFacts/file_metadata.pl postProcessingQueries.pl > $plPost
-swipl --quiet -t "ignore(allFilePaths),halt(1)" --consult-file $plPost | sort | uniq > $inputDirectory/temporaryFiles/allFilePaths.out
+#we want to process the parents of file paths discovered in dynamic analysis too, so we need to include the results of the symlink resolution script
+#cat $inputDirectory/prologFacts/file_metadata.pl postProcessingQueries.pl > $plPost
+cat $inputDirectory/prologFacts/symlinks_resolved_backtrace_results_and_dynamic_data.pl $inputDirectory/prologFacts/file_metadata.pl postProcessingQueries.pl > $plPost
+swipl --quiet -t "ignore(allMetaDataFilePaths),halt(1)" --consult-file $plPost | sort | uniq > $inputDirectory/temporaryFiles/allFilePaths.out
+swipl --quiet -t "ignore(allParameterFilePaths),halt(1)" --consult-file $plPost | sort | uniq >> $inputDirectory/temporaryFiles/allFilePaths.out
+swipl --quiet -t "ignore(allAccessedFilePaths),halt(1)" --consult-file $plPost | sort | uniq >> $inputDirectory/temporaryFiles/allFilePaths.out
+swipl --quiet -t "ignore(allExtensionFilePaths),halt(1)" --consult-file $plPost | sort | uniq >> $inputDirectory/temporaryFiles/allFilePaths.out
+cat $inputDirectory/temporaryFiles/allFilePaths.out | sort | uniq > $inputDirectory/temporaryFiles/uniquePaths.temp
+mv $inputDirectory/temporaryFiles/uniquePaths.temp $inputDirectory/temporaryFiles/allFilePaths.out
 ./findParents.py $inputDirectory/temporaryFiles/allFilePaths.out | sort | uniq > $inputDirectory/prologFacts/dirParents.pl
 
 swipl --quiet -t "ignore(prologFriendlyPermissionFacts),halt(1)" --consult-file $plPost | sort | uniq > $inputDirectory/prologFacts/prologFriendlyPermissions.pl
