8923ea701d073f24ad0ce3945d6a0f019f25c2c0
Razvan Deaconescu <razvan.deaconescu@cs.pub.ro>
2017-07-28 16:56:08 +0300

    automatedprocessing: Print number of unsandboxed executables in query

--- automatedProcessing/scripts/num-unsandboxed-executables/queries.pl
+++ automatedProcessing/scripts/num-unsandboxed-executables/queries.pl
@@ -2,6 +2,4 @@
 
 :- use_module(library(regex)).
 
-print_sandbox_profiles :- findall(P,usesSandbox(_,profile(P),_),L),sort(L,Lunique),maplist(writeln,Lunique).
-
-num_unsandboxed_executables :- findall(F,(file(fileType(T),filePath(F)),T =~ "Mach-O",not(usesSandbox(processPath(F),_,_))),L),sort(L,L2),length(L2,N).
+num_unsandboxed_executables :- findall(F,(file(fileType(T),filePath(F)),T =~ "Mach-O",not(usesSandbox(processPath(F),_,_))),L),sort(L,L2),length(L2,N),writeln(N).
