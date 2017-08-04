3c5cd8e0ed28e72f388f7dd8d9888b1dc0580866
Razvan Deaconescu <razvan.deaconescu@cs.pub.ro>
2017-08-04 11:42:35 +0300

    automatedProcessing: Add script to test speed of fail vs findall

--- automatedProcessing/scripts/executables-using-container/queries.pl
+++ automatedProcessing/scripts/executables-using-container/queries.pl
@@ -1,3 +1,4 @@
 :- [all_facts,all_rules].
 
 executables_using_sandbox_profile(P) :- findall(F,(file(fileType(T),filePath(F)),T =~ "Mach-O",usesSandbox(processPath(F),profile(P),_)),L),sort(L,L2),maplist(writeln,L2).
+executables_using_sandbox_profile2(P) :- file(fileType(T),filePath(F)),T =~ "Mach-O",usesSandbox(processPath(F),profile(P),_),writeln(F),fail.
