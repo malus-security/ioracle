d4619d902a9053b446e03575fc9651c83f7d2170
Razvan Deaconescu <razvan.deaconescu@cs.pub.ro>
2017-08-07 00:19:29 +0300

    automatedProcessing: Update script to extract files writable by mobile

--- automatedProcessing/scripts/files-writable-by-mobile/queries.pl
+++ automatedProcessing/scripts/files-writable-by-mobile/queries.pl
@@ -1,3 +1,3 @@
 :- [all_facts,all_rules].
 
-files_writable_by_user(U) :- findall(F,(unixAllow(puid(U),pgid(G),coarseOp("write"),F),user(userName("mobile"),_,userID(U),_,_,_,_),groupMembership(user("mobile"),_,groupIDNumber(G))),L),maplist(writeln,L).
+files_writable_by_user(U) :- findall(F,(user(userName(U),_,userID(Uid),_,_,_,_),unixUserAllow(uid(Uid),coarseOp("write"),file(F))),L),sort(L,L2),maplist(writeln,L2).
