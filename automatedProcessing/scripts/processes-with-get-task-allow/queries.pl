bd76257ef2e2b55309e174a3434ba0d25a1a509a
Razvan Deaconescu <razvan.deaconescu@cs.pub.ro>
2017-08-09 17:21:11 +0300

    automatedProcessing: Look for entitlements whose value is not false

--- automatedProcessing/scripts/processes-with-get-task-allow/queries.pl
+++ automatedProcessing/scripts/processes-with-get-task-allow/queries.pl
@@ -1,3 +1,3 @@
 :- [all_facts,all_rules].
 
-processes_with_entitlement_not_in_container(E) :- findall(P,(processSignature(filePath(P),_),processEntitlement(filePath(P),entitlement(key(E),_)),not(usesSandbox(processPath(P),profile("container"),_))),L),sort(L,L2),maplist(writeln,L2).
+processes_with_entitlement_not_in_container(E) :- findall(P,(processSignature(filePath(P),_),processEntitlement(filePath(P),entitlement(key(E),value(V))),not(usesSandbox(processPath(P),profile("container"),_)),not(V=bool("false"))),L),sort(L,L2),maplist(writeln,L2).
