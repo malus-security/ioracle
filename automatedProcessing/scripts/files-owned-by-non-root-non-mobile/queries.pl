1c65c7a823ed159773d5b3089d5a030edca870fe
Razvan Deaconescu <razvan.deaconescu@cs.pub.ro>
2017-08-10 14:53:57 +0300

    automatedProcessing: Fix scripts to extract user owned files
    Use userID predicate instead of userId.

--- automatedProcessing/scripts/files-owned-by-non-root-non-mobile/queries.pl
+++ automatedProcessing/scripts/files-owned-by-non-root-non-mobile/queries.pl
@@ -1,3 +1,3 @@
 :- [all_facts,all_rules].
 
-files_owned_by_non_root_non_mobile :- findall(F,(not(fileOwnerUserNumber(userNumber(Uid),F),user(userName("root"),_,userId(Uid),_,_,_,_)),not(fileOwnerUserNumber(userNumber(Uid),F),user(userName("mobile"),_,userId(Uid),_,_,_,_))),L),maplist(writeln,L).
+files_owned_by_non_root_non_mobile :- findall(F,(fileOwnerUserNumber(userNumber(Uid),F),not(user(userName("root"),_,userID(Uid),_,_,_,_)),not(user(userName("mobile"),_,userID(Uid),_,_,_,_))),L),maplist(writeln,L).
--- automatedProcessing/scripts/files-owned-by-root/queries.pl
+++ automatedProcessing/scripts/files-owned-by-root/queries.pl
@@ -1,3 +1,3 @@
 :- [all_facts,all_rules].
 
-files_owned_by_user(U) :- findall(F,(fileOwnerUserNumber(userNumber(Uid),F),user(userName(U),_,userId(Uid),_,_,_,_)),L),maplist(writeln,L).
+files_owned_by_user(U) :- findall(F,(fileOwnerUserNumber(userNumber(Uid),F),user(userName(U),_,userID(Uid),_,_,_,_)),L),maplist(writeln,L).
