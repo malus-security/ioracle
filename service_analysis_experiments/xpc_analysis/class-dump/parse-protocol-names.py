53b282ad6c0498914d9c178fc377d694b64853d9
Razvan Deaconescu <razvan.deaconescu@cs.pub.ro>
2018-07-03 23:00:50 +0300

    service_analysis: class-dump parsing scripts work on multiple protocol interfaces

--- service_analysis_experiments/xpc_analysis/class-dump/parse-protocol-names.py
+++ service_analysis_experiments/xpc_analysis/class-dump/parse-protocol-names.py
@@ -20,6 +20,9 @@ import pickle
 
 CONFIG_FILE = "protocols.pk"
 
+STATE_NONE = 0
+STATE_INTERFACE = 1
+STATE_PROTOCOL = 2
 
 def usage():
     print >>sys.stderr, "Usage: {} executable_filename".format(sys.argv[0])
@@ -32,38 +35,69 @@ def main():
 
     exec_file = sys.argv[1]
 
-    with open(CONFIG_FILE, "rb") as f:
-        try:
-            dictionary = pickle.load(f)
-        except (pickle.PickleError, EOFError):
-            dictionary = {}
+    try:
+        with open(CONFIG_FILE, "rb") as f:
+            try:
+                dictionary = pickle.load(f)
+            except (pickle.PickleError, EOFError):
+                dictionary = {}
+    except (IOError):
+        dictionary = {}
 
     dictionary[exec_file] = {}
 
     # Read input file.
-    inputText = sys.stdin.read().strip().replace("\n","").replace("@end","@end\n")
+    inputText = sys.stdin.read().strip(); # .replace("\n","").replace("@end","@end\n")
+    #print inputText
 
     # Find the names of the XPC protocols based on pattern of using NSXPCConnection.
+    state = STATE_NONE
+    nsxpc_protocols = set()
+    nsxpc_present = False
     for line in inputText.split('\n'):
-        protocolPattern = re.compile('\@interface\ (.*?)\ \:\ NSObject \<(.*?)\>\{.*NSXPCConnection.*\;.*?\}')
-        protocolMatch = re.match(protocolPattern, line)
-        if protocolMatch != None:
-            dictionary[exec_file][protocolMatch.group(2)] = []
+        if re.match('^@interface +.+ *: *.+ +\<.+\>$', line):
+            nsxpc_present = False
+            state = STATE_INTERFACE
+            parts = re.split('[\<\>]+', line)
+            protocols = re.split('[, ]+', parts[1])
+            continue
+        if re.match('^@end$', line):
+            if state == STATE_INTERFACE:
+                if nsxpc_present == True:
+                    nsxpc_protocols.update(protocols)
+                    nsxpc_present = False
+            state = STATE_NONE
+            continue
+        if state == STATE_INTERFACE:
+            if re.match('^ *NSXPCListener .*;$', line) or re.match('^ *NSXPCConnection .*;$', line):
+                nsxpc_present = True
+            continue
 
-    # Find the methods and their arguments for each XPC protocol.
-    for protName in dictionary[exec_file]:
-        for line in inputText.split('\n'):
-            protocolPattern = re.compile('\@protocol\ '+protName+'(.*)\@end')
-            protocolMatch = re.match(protocolPattern, line)
-            # If an XPC protocol header is detected, parse out the methods and arguments.
-            if protocolMatch != None:
-                coarseMatch = protocolMatch.group(1).replace(";",";\n")
-                # Results are stored in the dictionary.
-                dictionary[exec_file][protName] = coarseMatch.split("\n")[:-1]
+    state = STATE_NONE
+    content = ""
+    current_protocol = None
+    for line in inputText.split('\n'):
+        if re.match('^@protocol +.* +\<.*\>$', line):
+            protocol = re.split(' +', line)[1]
+            if protocol in nsxpc_protocols:
+                state = STATE_PROTOCOL
+                current_protocol = protocol
+                print "current_protocol: ", current_protocol
+                content = line + '\n'
+            continue
+        if re.match('^@end$', line):
+            if state == STATE_PROTOCOL:
+                content += (line + '\n')
+                dictionary[exec_file][current_protocol] = content
+                content = ""
+                current_protocol = None
+            state = STATE_NONE
+            continue
+        if state == STATE_PROTOCOL:
+            content += (line + '\n')
 
     with open(CONFIG_FILE, "wb") as f:
         pickle.dump(dictionary, f)
-
 
 if __name__ == "__main__":
     sys.exit(main())
