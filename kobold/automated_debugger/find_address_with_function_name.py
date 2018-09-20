c955e7ba1fce76ce59343e0191b4f0ed21ba1cda
alecdeshotels <ladeshot@ncsu.edu>
2018-09-20 02:08:01 -0400

    added parser to transform a method declaration into IDA's function naming convention.

--- kobold/automated_debugger/find_address_with_function_name.py
+++ kobold/automated_debugger/find_address_with_function_name.py
@@ -12,10 +12,36 @@ import os
 import pickle
 import re
 
-functionName = "startMonitoringScenarioTriggerOfType:forClient:"
+#TODO I should extract the relevant methods from our dictionary and parse out the name likely to appear in the IDA disassembly.
+#functionName = "startMonitoringScenarioTriggerOfType:forClient:"
+#functionName =  "- (void)startMonitoringScenarioTriggerOfType:(unsigned long long)arg1 forClient:(byref id <CLRoutineMonitorClientProtocol>)arg2;"
+
+#Given a full method declaration, parse out a string that will match IDA's function naming convention
+
+#This function appears more than once.
+#I guess the same selector is used by multiple objects.
+#We can state this limitation in the paper. It shouldn't be a big deal.
+functionName =  "- (void)startUpdatingLocation;"
+#drop the semicolon
+functionName = functionName[:-1]
+funNameComponentList = []
+if ":" in functionName:
+  funNameComponentList = functionName.split(":")[:-1]
+  funNameComponentList = [comp + ":" for comp in funNameComponentList]
+
+else:
+  funNameComponentList = [functionName]
+  
+idaFunName = ""
+for component in funNameComponentList:
+  regexResult = re.match('.*[)\ ](.*)$',component)
+  idaFunName += regexResult.group(1)
+
+print idaFunName
+
 for nName in idautils.Names():
   name = nName[1]
-  if functionName in name:
+  if idaFunName in name:
     nameAddress = nName[0]
     if idc.get_segm_name(nameAddress) == "__text":
       print "address of function is: ",
