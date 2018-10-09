b541eec92be28c4c873743ad589c66f0ccc84ca0
Razvan Deaconescu <razvan.deaconescu@cs.pub.ro>
2018-10-09 18:48:24 +0300

    kobold:deploy-on-device: Use codesign for signing

--- kobold/entitlement_queries/deploy-on-device/add_entitlement_to_exec.sh
+++ kobold/entitlement_queries/deploy-on-device/add_entitlement_to_exec.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Sample run
+# ./add_entitlement_to_exec.sh  ~/Projects/ios-security-research/ios-command-line-tool.git/ios-command-line-tool-raw spi.xml my
+
 if test $# -ne 3; then
     echo "Usage: $0 <in_exec_file> <ent_file> <out_exec_file>" 1>&2
     exit 1
@@ -21,6 +24,8 @@ fi
 
 JTOOL="/Users/razvan/bin/jtool"
 
-$JTOOL -arch arm64 -e arch "$in_exec_file"
-JDEBUG=1 $JTOOL -arch arm64 --sign --ent "$ent_file" --inplace "$in_exec_file".arch_arm64
-mv "$in_exec_file".arch_arm64 "$out_exec_file"
+#$JTOOL -arch arm64 -e arch "$in_exec_file"
+#JDEBUG=1 $JTOOL -arch arm64 --sign --ent "$ent_file" --inplace "$in_exec_file".arch_arm64
+cp "$in_exec_file" "$out_exec_file"
+# XXX: Replace the parameter to the -fs option with the output from the `security find-identity` command.
+codesign -fs 8AE0658EE9349F3E8375D184BF4D3E160E82147E --entitlements "$ent_file" "$out_exec_file"
