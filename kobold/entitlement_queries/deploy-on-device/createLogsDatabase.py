cdc12f8cc96dd7621087605d0c6a1dbc55a9341d
Costin Carabas <costin.carabas@gmail.com>
2018-10-31 15:47:18 +0200

    kobold: createLogsDatabase minor fix
    Signed-off-by: Costin Carabas <costin.carabas@gmail.com>

--- kobold/entitlement_queries/deploy-on-device/createLogsDatabase.py
+++ kobold/entitlement_queries/deploy-on-device/createLogsDatabase.py
@@ -5,11 +5,11 @@ import re
 import os
 
 location = 'logs'
-table_name = 'appOutput'
-
+table_appOutput = 'appOutput'
+table_run = 'run'
 
 def init():
-    clean()
+    #clean()
     global conn
     global c
     conn = sqlite3.connect(location)
@@ -17,16 +17,29 @@ def init():
     create_database()
 
 def create_database():
-    sql = 'create table if not exists ' + table_name + ' ( \
+    sql = 'create table if not exists ' + table_appOutput + ' ( \
             id INTEGER NOT NULL, \
             method TEXT NOT NULL, \
             machPort TEXT NOT NULL, \
             hasCompletion INTEGER DEFAULT 0, \
             connectionInvalidated INTEGER DEFAULT 0, \
             connectionTerminated INTEGER DEFAULT 0, \
-            runID INTEGER NOT NULL)'
+            runID INTEGER)'
     c.execute(sql)
-    sql = 'create unique index u_id on ' + table_name + ' (id, method, machPort, hasCompletion)'
+    sql = 'create unique index if not exists u_id on ' + table_appOutput + ' (id, runID)'
+    c.execute(sql)
+
+    # Table for Device characteristics
+    sql = 'create table if not exists ' + table_run + ' ( \
+            id INTEGER PRIMARY KEY AUTOINCREMENT, \
+            model TEXT, \
+            os TEXT, \
+            jailbroken TEXT, \
+            ent_file TEXT, \
+            raw_file TEXT,\
+            runID INTEGER)'
+    c.execute(sql)
+    sql = 'create unique index if not exists u_id on ' + table_run + ' (model, os, runID)'
     c.execute(sql)
     conn.commit()
 
@@ -34,17 +47,34 @@ def clean():
     os.system('rm -rf ' + location)
 
 def insert_log(m_id, method, machPort, hasCompletion, connectionInvalidated, connectionTerminated, runID):
-    sql = "insert into " + table_name + " \
+    sql = "insert into " + table_appOutput + " \
                 (id, method, machPort, hasCompletion, connectionInvalidated, connectionTerminated, runID) values \
                 (%d, '%s', '%s', %d, %d, %d, %d)" % \
                 (m_id, method, machPort, hasCompletion, connectionInvalidated, connectionTerminated, runID)
     c.execute(sql)
     conn.commit()
 
+def insert_run(model, os, jailbroken, ent_file, raw_file, runID):
+    ent = open(ent_file, 'rb')
+    raw = open(raw_file, 'rb')
+
+    with ent:
+        ent_data = ent.read()
+    with raw:
+        raw_data = raw.read()
+    sql = "insert into " + table_run + " \
+                (model, os, jailbroken, ent_file, raw_file, runID) values \
+                ('%s', '%s', '%s', '%s', '%s', %d)" % \
+                (model, os, jailbroken, sqlite3.Binary(ent_data), sqlite3.Binary(raw_data), runID)
+    c.execute(sql)
+    conn.commit()
+
+
 def add_log(log_name):
     curr_dir = os.path.dirname(__file__)
     log = os.path.join(curr_dir, log_name)
     log_file = open(log, "r")
+    runID = get_next_runID()
 
     for m_id in range(1,2020):
         methodName, machPort, connectionTerminated, connectionInvalidated, hasCompletion = ["", "", 0, 0, 0]
@@ -66,16 +96,25 @@ def add_log(log_name):
                 hasCompletion = 1
                 continue
         insert_log(m_id, methodName, machPort, hasCompletion,
-                connectionInvalidated, connectionTerminated, get_next_runID())
+                connectionInvalidated, connectionTerminated, runID)
 
 def get_next_runID():
     sql = "select MAX(runID) from appOutput;"
     c.execute(sql)
     runID = c.fetchall()[0][0]
-    if runID:
-            return int(runID)
+    if runID == None:
+            return 0
 
-    return 0
+    return int(runID)+1
+
+
+def get_runID():
+    sql = "select MAX(runID) from appOutput;"
+    c.execute(sql)
+    runID = c.fetchall()[0][0]
+    if runID == None:
+            return int(0)
+
+    return int(runID)
 
 init()
-add_log("results/app_logs/filemon/run.out")
