1f817340fe6ae12920d2ee47b209efb2213eb2bd
Malus Security <cristian.radu00@yahoo.com>
2023-04-09 18:25:21 +0300

    decode after changes are made

--- automatedDataExtraction/scriptsToAutomate/sanitizeFilePaths.py
+++ automatedDataExtraction/scriptsToAutomate/sanitizeFilePaths.py
@@ -10,12 +10,16 @@ facts = fdata.split(b"\n")
 for line in facts:
   #print line
   #pattern = re.compile(',filepath\(\"(.*)\"\)\)\.')
-  pattern = re.compile(b'(^.*,filePath\(\")(.*)(\"\)\)\.$)')
+  pattern = b'(^.*,filePath\(\")(.*)(\"\)\)\.$)'
+  pattern = re.compile(pattern)
   #pattern = re.compile('^.*,filepath\(.*')
   match = pattern.match(line)
   filePath = match.group(2)
   #print filePath
   sanitizedPath = filePath.replace(b'"',bytes('_DOUBLEQUOTEWASHERE_', 'utf-8')).replace(b'\\',bytes('_BACKSLASHWASHERE_', 'utf-8'))
+  #sanitizedPath = filePath.replace('"','_DOUBLEQOUTEWASHERE_').replace('\\', '_BACKSLASHWASHERE_')
   #print sanitizedPath
   sanitizedLine = match.group(1) + sanitizedPath + match.group(3)
+  sanitizedLine = sanitizedLine.decode('utf-8')
   #print sanitizedLine
+  print(sanitizedLine)
