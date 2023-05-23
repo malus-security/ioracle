cc682d46ed838715590412a6dac24faca21a1955
Corekore <cristian.radu00@yahoo.com>
2023-05-23 21:49:34 +0300

    shellchecked

--- automatedDataExtraction/scriptsToAutomate/stringExtractor.sh
+++ automatedDataExtraction/scriptsToAutomate/stringExtractor.sh
@@ -8,32 +8,28 @@ fi
 rootfs_path="$1/"
 rootfs_path=${rootfs_path//\/\//\/}
 
-#the find command also has a printf option and provides much of the same data as stat
-IFS=$'\n'
+#instead of IFS=$'\n', just read -r
+while read -r line; do
+  #./FileSystem is hardcoded and should be changed for other systems.
+  #this should really be a parameter passed in as a command line argument...
+  filePath="$rootfs_path$line"
 
-echoerr() { echo "$@" 1>&2; }
+  #echo "about to process $line"
 
-while read line; do
-    #./FileSystem is hardcoded and should be changed for other systems.
-    #this should really be a parameter passed in as a command line argument...
-    filePath="$rootfs_path$line"
+  #I think this will only work if I set the minimum string length to a reasonably high number.
+  #otherwise, I get a bunch of junk...
+  #I wonder if IDA has a smarter way to remove strings that are not of interest.
+  #for now I am limiting the results to strings of 7 or more ascii characters.
+  #the strings must have at least three consecutive numbers or letters.
+  #the strings must not contain backslashes or double quotes
+  #thisSetOfStrings=$(strings -n 7 $filePath | grep '[a-zA-Z0-9][a-zA-Z0-9][a-zA-Z0-9]' | grep -v '"' | grep -v '\\\\')
 
-    #echo "about to process $line"
-
-    #I think this will only work if I set the minimum string length to a reasonably high number.
-    #otherwise, I get a bunch of junk...
-    #I wonder if IDA has a smarter way to remove strings that are not of interest.
-    #for now I am limiting the results to strings of 7 or more ascii characters.
-    #the strings must have at least three consecutive numbers or letters.
-    #the strings must not contain backslashes or double quotes
-    thisSetOfStrings=`strings -n 7 $filePath | grep '[a-zA-Z0-9][a-zA-Z0-9][a-zA-Z0-9]' | grep -v '"' | grep -v '\\\\'`
-
-    #echo "about to iterate through strings"
-    #I'm not sure why, but the while loop works, and the for loop causes the script to fail.
-    #It may have something to do with memory requirements and how for loops work in bash.
-    #for stringEntry in $thisSetOfStrings; do
-    strings -n 7 $filePath | grep '[a-zA-Z0-9][a-zA-Z0-9][a-zA-Z0-9]' | grep -v '"' | grep -v '\\\\' | while read stringEntry; do
-        #echo "about to output a prolog fact"
-        echo "processString(filePath(\"$line\"),stringFromProgram(\"$stringEntry\"))."
-    done
+  #echo "about to iterate through strings"
+  #I'm not sure why, but the while loop works, and the for loop causes the script to fail.
+  #It may have something to do with memory requirements and how for loops work in bash.
+  #for stringEntry in $thisSetOfStrings; do
+  strings -n 7 "$filePath" | grep '[a-zA-Z0-9][a-zA-Z0-9][a-zA-Z0-9]' | grep -v '"' | grep -v '\\\\' | while read -r stringEntry; do
+    #echo "about to output a prolog fact"
+    echo "processString(filePath(\"$line\"),stringFromProgram(\"$stringEntry\"))."
+  done
 done
